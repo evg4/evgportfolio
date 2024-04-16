7ead5b3febcc395ae2c9ef70fa2159cf8b8b9f5a
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-04-16 19:11:37 +0100

    Improve Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,9 +5,6 @@ import journey from "../../assets/stepping-stones2.jpg";
 function Contact() {
   return (
     <section className={styles.contact}>
-      <div className={styles.contact1}>
-        <h2>Contact</h2>
-      </div>
       <div className={styles.contact2}>
         {" "}
         <p>Let's keep in touch.</p>
@@ -21,7 +18,6 @@ function Contact() {
           })}
         </div>
       </div>
-      <div className={styles.contact3}></div>
     </section>
   );
 }
