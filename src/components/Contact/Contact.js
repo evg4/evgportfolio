ee955f9bb42fd07a9d2618b61d6c448527682eb4
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-04-16 19:00:49 +0100

    Add Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,28 @@
+import socials from "../../data/socials";
+import styles from "./Contact.module.css";
+import journey from "../../assets/stepping-stones2.jpg";
+
 function Contact() {
   return (
-    <>
-      <p>This will be the contact page</p>
-    </>
+    <section className={styles.contact}>
+      <div className={styles.contact1}>
+        <h2>Contact</h2>
+      </div>
+      <div className={styles.contact2}>
+        {" "}
+        <p>Let's keep in touch.</p>
+        <div className={styles.socials}>
+          {socials.map((social) => {
+            return (
+              <a href={social.link} target="blank">
+                <img src={social.logo}></img>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.contact3}></div>
+    </section>
   );
 }
 
---
+++ src/data/socials.js
@@ -0,0 +1,28 @@
+import github from "../assets/github-mark.png";
+import linkedin from "../assets/LI-In-Bug.png";
+import shecodes from "../assets/shecodes.png";
+import x from "../assets/logo-black.png";
+import codecademy from "../assets/codecademy-icon.svg";
+
+const socials = [
+  {
+    site: "LinkedIn",
+    logo: linkedin,
+    link: "https://www.linkedin.com/in/elizabeth-v-gardiner/",
+  },
+  { site: "GitHub", logo: github, link: "https://github.com/evg4" },
+
+  { site: "X", logo: x, link: "https://twitter.com/Lizzie_Codes" },
+  {
+    site: "Codecademy",
+    logo: codecademy,
+    link: "https://www.codecademy.com/profiles/Lizzie_Codes",
+  },
+  {
+    site: "SheCodes",
+    logo: shecodes,
+    link: "https://www.shecodes.io/graduates/81146-elizabeth-gardiner",
+  },
+];
+
+export default socials;
