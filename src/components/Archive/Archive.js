92380d0762ffeb82e605b67ccaeb6bfa2dfcd00b
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-04-19 23:04:04 +0100

    Styling for Archive component

--- src/components/Archive/Archive.js
+++ src/components/Archive/Archive.js
@@ -2,24 +2,55 @@ import styles from "./Archive.module.css";
 import blogs from "../../data/blogs";
 import { years } from "../../data/blogs";
 
+let monthsObj = {
+  "01": "January",
+  "02": "February",
+  "03": "March",
+  "04": "April",
+  "05": "May",
+  "06": "June",
+  "07": "July",
+  "08": "August",
+  "09": "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
 function Archive() {
-  return years.map((year) => {
-    return (
-      <div key={year.year}>
-        <p>Year: {year.year}</p>
-        {year.months.map((month) => {
-          return (
-            <div key={year.year + month.month}>
-              <p>Month: {month.month}</p>
-              {month.days.map((day) => {
-                return <p key={year.year + month.month + day}>Day: {day}</p>;
+  return (
+    <section className={styles.archive}>
+      {years.map((year) => {
+        return (
+          <div key={year.year}>
+            <details className={styles.year}>
+              <summary>{year.year}</summary>
+              {year.months.map((month) => {
+                return (
+                  <div key={year.year + month.month}>
+                    <details className={styles.month}>
+                      <summary>{monthsObj[month.month]}</summary>
+
+                      {month.days.map((day) => {
+                        return (
+                          <p
+                            className={styles.day}
+                            key={year.year + month.month + day}
+                          >
+                            {`${day}.${month.month}.${year.year}`}
+                          </p>
+                        );
+                      })}
+                    </details>
+                  </div>
+                );
               })}
-            </div>
-          );
-        })}
-      </div>
-    );
-  });
+            </details>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
 /*function Archive() {
