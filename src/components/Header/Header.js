59d412020b09cd1c8e5efd2ad0b4e94a74a17616
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-05-24 21:08:18 +0100

    style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import styles from "./Header.module.css";
 import Nav from "../Nav/Nav";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <h1>Lizzie Codes</h1>
+      <Link to="/">
+        <h1>Lizzie Codes</h1>
+      </Link>
+
       <Nav />
     </header>
   );
