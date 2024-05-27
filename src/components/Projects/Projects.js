994ee317a7376cbc5d7895f140ca37a3608e8e6c
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-05-27 22:07:22 +0100

    update Projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,17 +1,35 @@
 import styles from "./Projects.module.css";
 import projects from "../../data/projects";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Projects() {
+  const navigate = useNavigate();
+  function handleClick(e) {
+    window.open(e.currentTarget.id);
+  }
+
   return (
     <section className={styles.projectsSection}>
       <h2>Projects</h2>
+      <p>
+        Here is a selection of my favourite projects. Click on a tile to visit
+        the page and see it in action! These are the ones I'm most proud of, but
+        if you want to see more you can visit my <Link to="/blog">blog</Link>.
+      </p>
       <div className={styles.tiles}>
         {projects.map((project) => {
           return (
-            <div className={styles.tile}>
+            <div
+              key={project.title}
+              id={project.link}
+              onClick={handleClick}
+              className={styles.tile}
+            >
               <h3>{project.title}</h3>
               <p className={styles.description}>{project.description}</p>
-              <img src={project.image}>{/*add image source!*/}</img>
+              <img src={project.image}></img>
               <p>
                 <strong>Date:</strong> {project.date}
               </p>
