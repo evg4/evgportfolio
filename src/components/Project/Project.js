04990a4b1492dfed083b544ddc4a6c9879d5e59a
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-06-16 16:32:57 +0100

    Make Projects modular

---
+++ src/components/Project/Project.js
@@ -0,0 +1,40 @@
+import styles from "./Project.module.css";
+import projects from "../../data/projects";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function Project() {
+  const navigate = useNavigate();
+  function handleClick(e) {
+    window.open(e.currentTarget.id);
+  }
+
+  return (
+    <section className={styles.projectsSection}>
+      <div className={styles.tiles}>
+        {projects.map((project) => {
+          return (
+            <div
+              key={project.title}
+              id={project.link}
+              onClick={handleClick}
+              className={styles.tile}
+            >
+              <h3>{project.title}</h3>
+              <p className={styles.description}>{project.description}</p>
+              <img src={project.image}></img>
+              <p>
+                <strong>Date:</strong> {project.date}
+              </p>
+              <p>
+                <strong>Languages and frameworks:</strong> {project.languages}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default Project;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,45 +1,19 @@
 import styles from "./Projects.module.css";
 import projects from "../../data/projects";
+import Project from "../Project/Project";
 import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function Projects() {
-  const navigate = useNavigate();
-  function handleClick(e) {
-    window.open(e.currentTarget.id);
-  }
-
   return (
     <section className={styles.projectsSection}>
       <h2>Projects</h2>
       <p>
         Here is a selection of my favourite projects. Click on a tile to visit
         the page and see it in action! These are the ones I'm most proud of, but
-        if you want to see more you can visit my <Link to="/blog">blog</Link>.
+        if you want to see more you can <Link to="/blog">visit my blog</Link>.
       </p>
-      <div className={styles.tiles}>
-        {projects.map((project) => {
-          return (
-            <div
-              key={project.title}
-              id={project.link}
-              onClick={handleClick}
-              className={styles.tile}
-            >
-              <h3>{project.title}</h3>
-              <p className={styles.description}>{project.description}</p>
-              <img src={project.image}></img>
-              <p>
-                <strong>Date:</strong> {project.date}
-              </p>
-              <p>
-                <strong>Languages and frameworks:</strong> {project.languages}
-              </p>
-            </div>
-          );
-        })}
-      </div>
+      <Project />
     </section>
   );
 }
