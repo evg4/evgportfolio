2eb4ec001846dda1b1e3ce416a0d77251ebb1c1c
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-09-07 15:57:40 +0100

    add projects data

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import BackButton from "../BackButton/BackButton";
 import PreviousButton from "../PreviousButton/PreviousButton";
 import NextButton from "../NextButton/NextButton";
+import parse from "html-react-parser";
 
 function ProjectPage() {
   let { title } = useParams();
@@ -29,7 +30,7 @@ function ProjectPage() {
       <video src={project.video} width="80%" controls>
         Video not supported
       </video>
-      <p>{project.details}</p>
+      <p>{parse(project.details)}</p>
       <div className={styles.buttons}>
         {" "}
         <BackButton newLink="/projects" newLinkText="Back to all projects" />
